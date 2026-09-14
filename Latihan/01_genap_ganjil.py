845da13e4caa758a33fb0d454d1673324c1b6ffb
Riki Agil Saputra <rikiagilsaputra151108@gmail.com>
2026-09-14 20:57:32 +0700

    Menyelesaikan program latihan 1 dan 2

---
+++ Latihan/01_genap_ganjil.py
@@ -0,0 +1,9 @@
+#Memasukkan bilangan yang diinginkan 
+bilangan = int(input("Masukkan bilangan bulat:"))
+
+#logika percabangan untuk menentukan bilangan genap atau ganjil
+if bilangan % 2 == 0:
+    print (f"{bilangan} adalah bilangan genap")
+else:
+      print (f"{bilangan} adalah bilangan ganjil")
+
---
+++ Latihan/02_bandingkan_dua_bilangan.py
@@ -0,0 +1,15 @@
+#Memasukkan bilangan yang diinginkan
+a = float(input("Masukkan bilangan pertama: "))
+b = float(input("Masukkan bilangan kedua: "))
+
+#logika percabangan untuk membandingkan kedua bilangan
+if a >= b:
+    if a == b:
+        #Menampilkan hasil jika kedua bilangan sama
+        print("kedua bilangan sama")
+    else:
+        #Menampilkan hasil perbandingan bilangan pertama dan bilangan kedua
+        print("bilangan pertama lebih besar")
+else:
+    #Menampilkan hasil perbandingan bilangan pertama dan bilangan kedua
+    print("bilangan pertama lebih kecil")
